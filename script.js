27785391a13bfa4c4429d395bf734fb5d098087d
Le Port Enzo <61596885+EnzoLePort@users.noreply.github.com>
2021-12-19 17:43:34 +0100

    first enigme

---
+++ script.js
@@ -0,0 +1,5 @@
+const clues = ["The FIRST is the color of the SUN", "the SECOND is the color of the THUNDER", "the THIRD is the color of LOVE", "the LAST is the color of the SKY"];
+
+function giveClues() {
+    window.alert(clues[Math.floor(Math.random() * 4)]);
+}
