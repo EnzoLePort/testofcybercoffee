4868b875a4641ff119b02e8946c1f7c742566a30
Le Port Enzo <61596885+EnzoLePort@users.noreply.github.com>
2022-01-14 14:39:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const clues = ["The FIRST is the color of the SUN", "the SECOND is the color of the THUNDER", "the THIRD is the color of LOVE", "the LAST is the color of the SKY"];
+const clues = ["The FIRST is the color of the SUN", "the SECOND is the color of the ANGER", "the THIRD is the color of LOVE", "the LAST is the color of the SKY"];
 
 function giveClues() {
     window.alert(clues[Math.floor(Math.random() * 4)]);
